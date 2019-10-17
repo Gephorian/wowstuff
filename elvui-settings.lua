5fe17ebf1322b34d256a2c0ff5586e754aeeac8b
Adam Coleman <gephorian@gmail.com>
2019-10-17 09:15:04 -0500

    More tweaking

--- elvui-settings.lua
+++ elvui-settings.lua
@@ -1,6 +1,16 @@
 {
     ["currentTutorial"] = 3,
     ["general"] = {
+        ["bordercolor"] = {
+            ["b"] = 0,
+            ["g"] = 0,
+            ["r"] = 0,
+        },
+        ["backdropfadecolor"] = {
+            ["b"] = 0.054,
+            ["g"] = 0.054,
+            ["r"] = 0.054,
+        },
         ["minimap"] = {
             ["size"] = 215,
         },
@@ -8,21 +18,21 @@
     ["movers"] = {
         ["ElvUF_AssistMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,126,757",
         ["ElvUF_Raid40Mover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,497",
-        ["ShiftAB"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,901,57",
+        ["ElvAB_4"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-4,258",
         ["PetAB"] = "BOTTOM,ElvUIParent,BOTTOM,0,23",
         ["ElvAB_1"] = "BOTTOM,ElvUIParent,BOTTOM,0,58",
         ["ElvAB_2"] = "BOTTOM,ElvUIParent,BOTTOM,0,137",
         ["GMMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-272,-4",
         ["TimeManagerFrameMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-4,-56",
         ["QuestTimerFrameMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-122,-377",
-        ["ElvAB_4"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-4,258",
-        ["ElvUF_TargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,236,212",
+        ["ElvUIBankMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-512,4",
+        ["MinimapMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-452,4",
         ["ElvAB_3"] = "BOTTOM,ElvUIParent,BOTTOM,0,98",
-        ["LootFrameMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-599,366",
+        ["ElvAB_5"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-40,258",
         ["QuestWatchFrameMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-151,-450",
         ["ElvUF_PetMover"] = "BOTTOM,ElvUIParent,BOTTOM,-331,135",
-        ["MinimapMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-435,4",
         ["ElvUF_RaidpetMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,736",
+        ["ShiftAB"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,901,57",
         ["BNETMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-4,-4",
         ["ElvNP_PlayerMover"] = "TOP,ElvUIParent,TOP,0,-485",
         ["ElvUF_RaidMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,497",
@@ -30,19 +40,19 @@
         ["ElvUF_TargetTargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,258",
         ["TooltipMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-80,186",
         ["ElvUF_TankMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,757",
-        ["DurabilityFrameMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-347,215",
+        ["DurabilityFrameMover"] = "BOTTOM,ElvUIParent,BOTTOM,313,78",
         ["ElvUF_PlayerMover"] = "BOTTOM,ElvUIParent,BOTTOM,-235,212",
-        ["ElvAB_5"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-40,258",
+        ["ElvUF_TargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,236,212",
         ["ElvUF_PartyMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,260",
         ["AlertFrameMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,338",
-        ["ElvUIBankMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-580,4",
+        ["LootFrameMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-599,366",
         ["TaxiButtonMover"] = "BOTTOM,ElvUIParent,BOTTOM,-268,66",
     },
     ["bags"] = {
         ["bagWidth"] = 258,
         ["split"] = {
-            ["player"] = true,
             ["bag4"] = true,
+            ["player"] = true,
             ["bag3"] = true,
             ["bank"] = true,
         },
@@ -51,9 +61,27 @@
         ["sortInverted"] = false,
     },
     ["chat"] = {
+        ["panelHeight"] = 240,
         ["panelColorConverted"] = true,
     },
     ["unitframe"] = {
+        ["colors"] = {
+            ["auraBarBuff"] = {
+                ["b"] = 0.1,
+                ["g"] = 0.1,
+                ["r"] = 0.1,
+            },
+            ["castColor"] = {
+                ["b"] = 0.1,
+                ["g"] = 0.1,
+                ["r"] = 0.1,
+            },
+            ["health"] = {
+                ["b"] = 0.1,
+                ["g"] = 0.1,
+                ["r"] = 0.1,
+            },
+        },
         ["units"] = {
             ["player"] = {
                 ["debuffs"] = {
